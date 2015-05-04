b54d802cfec2cce0b31871d6aef200ccec5540fb
Justin <justincyork@gmail.com>
2015-05-04 19:16:49 +0000

    removePersonFSConnection

--- test/removePersonFSConnection.js
+++ test/removePersonFSConnection.js
@@ -3,20 +3,19 @@ require('./setup');
 var wikitree = require('./../lib/wikitree'),
     expect = require('chai').expect;
     
-describe('addPersonFSConnection', function(){
+describe('removePersonFSConnection', function(){
   
   it('error', function(){
     expect(function(){
-      wikitree.addPersonFSConnection();
+      wikitree.removePersonFSConnection();
     }).to.throw(Error);
   });
   
   it('success', function(done){
-    wikitree.addPersonFSConnection(9889663, 'KW42-C5C', '2015-04-01 21:35:57', 'certain').done(function(connection){
+    wikitree.removePersonFSConnection(9889663, 'KWWW-C5C').done(function(connection){
       check(done, function(){
         expect(connection.mUserId).to.equal('9889663');
-        expect(connection.mFSId).to.equal('KW42-C5C');
-        expect(connection.mFSModified).to.equal('2015-04-01 21:35:57');
+        expect(connection.mFSId).to.equal('KWWW-C5C');
         expect(connection.mCertainty).to.equal('certain');
       });
     });
---
+++ test/responses/removePersonFSConnection.js
@@ -0,0 +1,20 @@
+module.exports = function(opts){
+  if(opts.key == '9889663' && opts.fs_id === 'KWWW-C5C'){
+    return [
+      {
+        "user_id": "9889663",
+        "status": 0,
+        "connection": {
+          "mUserId": "9889663",
+          "mFSId": "KWWW-C5C",
+          "mAddedBy": "4650952",
+          "mAddedOn": "2015-05-04 18:55:42",
+          "mUpdatedBy": "4650952",
+          "mUpdatedOn": "2015-05-04 18:55:42",
+          "mFSModified": "2015-04-01 21:35:57",
+          "mCertainty": "certain"
+        }
+      }
+    ];
+  }
+};
