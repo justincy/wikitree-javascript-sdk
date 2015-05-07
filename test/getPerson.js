7302f7be4bd805efb8527b48391210881ef8a968
Justin <justincyork@gmail.com>
2015-05-07 15:42:48 +0000

    better test coverage of Person

--- test/getPerson.js
+++ test/getPerson.js
@@ -13,6 +13,16 @@ describe('getPerson', function(){
         expect(person.getMother()).to.be.instanceof(wikitree.Person);
         expect(person.getFather()).to.be.instanceof(wikitree.Person);
         expect(person.getChildren()).to.have.all.keys(['10147688','10147705']);
+        expect(person.getFirstName()).to.equal('Mary');
+        expect(person.getLastNameCurrent()).to.equal('Wojnowski');
+        expect(person.getGender()).to.equal('Female');
+        expect(person.getBirthDate()).to.equal('1863-00-00');
+        expect(person.getBirthLocation()).to.equal('Poznań, Poland');
+        expect(person.getDeathDate()).to.equal('1939-10-14');
+        expect(person.getDeathLocation()).to.equal('Amsterdam, Montgomery, New York, United States');
+        expect(person.getId()).to.equal(10147690);
+        expect(person.isLiving()).to.be.false;
+        expect(person.getName()).to.equal('Wojnowski-3');
       });
     });
   });
